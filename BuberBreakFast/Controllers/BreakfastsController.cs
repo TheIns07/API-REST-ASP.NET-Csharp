7918a75821c13154b1a0924e570a702e23d4b507
TheIns07 <arturoinscreup2401@hotmail.com>
2023-10-05 16:29:30 -0700

    CRUD completad. Build hecho

--- BuberBreakFast/Controllers/BreakfastsController.cs
+++ BuberBreakFast/Controllers/BreakfastsController.cs
@@ -3,15 +3,26 @@ using Microsoft.AspNetCore.Mvc;
 namespace BubberBreakFast.Controllers;
 
 [ApiController]
+[Route("[breakfasts]")]
 public class BreakfastsController: ControllerBase {
 
-    [HttpPost("/breakfasts")]
+    [HttpPost("/")]
     public IActionResult CreateBreakFast(CreateBreakfastRequest req){
         return Ok(req);
     }
 
-    [HttpGet("/breakfasts/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetBreakFast(Guid id){
+        return Ok(id);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult UpdateBreakFast(Guid id, UpdateBreakfastRequest req){
+        return Ok(req);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakFast(Guid id){
         return Ok(id);
     }
 
